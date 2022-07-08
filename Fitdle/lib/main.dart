8f48796b88ee990fe586ce35c4db80477f24bbb0
andrew <awu.dev@gmail.com>
2022-07-08 17:59:33 -0400

    add firebase library

--- Fitdle/lib/main.dart
+++ Fitdle/lib/main.dart
@@ -3,8 +3,15 @@ import 'package:fitdle/scene/signup/birthday_screen.dart';
 import 'package:fitdle/scene/signup/create_account_screen.dart';
 import 'package:fitdle/scene/signup/personal_info_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const Fitdle());
 }
 
