963e4dbb40fab83b8db632d881cc5d5e6a886ed2
andrew <awu.dev@gmail.com>
2022-07-21 03:09:01 -0400

    shift dates, switch graphs

--- Fitdle/lib/pages/analytics/analytics_screen.dart
+++ Fitdle/lib/pages/analytics/analytics_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'package:fitdle/components/common.dart';
+import 'package:fitdle/models/chart_data.dart';
 import 'package:fitdle/pages/analytics/analytics_vm.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fitdle/constants/all_constants.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class AnalyticsScreen extends StatefulWidget {
   const AnalyticsScreen({Key? key}) : super(key: key);
@@ -15,6 +17,8 @@ class AnalyticsScreen extends StatefulWidget {
 class _AnalyticsScreenState extends State<AnalyticsScreen> {
   late final AnalyticsVM _analyticsVM;
   late StreamSubscription _navigationSubscription;
+  late TooltipBehavior _tooltip;
+
   int selectedGraph = 0;
   Color rewardsColor = Colors.white;
   Color caloriesColor = Colors.purple;
@@ -23,6 +27,7 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
   void initState() {
     super.initState();
     _analyticsVM = AnalyticsVM();
+    _tooltip = TooltipBehavior(enable: true);
   }
 
   @override
@@ -48,9 +53,44 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
         padding: const EdgeInsets.fromLTRB(regular, regular, regular, 0),
         child: Column(
           children: [
-            buildSegmentedControl()
+            buildSegmentedControl(),
+            const SizedBox(height: 20),
+            buildWeekSwitch(),
+            const SizedBox(height: large),
+            SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              tooltipBehavior: _tooltip,
+              series: <ChartSeries<ChartData, String>>[
+                ColumnSeries<ChartData, String>(
+                    dataSource: _analyticsVM.selectedData,
+                    xValueMapper: (ChartData data, _) => data.x,
+                    yValueMapper: (ChartData data, _) => data.y,
+                    name: (selectedGraph == 0) ? "Earnings" : "Calories",
+                    color: Colors.deepPurpleAccent)
+              ])
           ]),
       ),
+    );
+  }
+
+  buildWeekSwitch() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        IconButton(
+            onPressed:() {
+              setState(() {_analyticsVM.moveDateBack();});
+            },
+            icon: const Icon(Icons.chevron_left, color: Colors.purple)
+        ),
+        fitdleText("${_analyticsVM.startDate} - ${_analyticsVM.endDate}", hint),
+        IconButton(
+            onPressed:() {
+              setState(() {_analyticsVM.moveDateForward();});
+            },
+            icon: const Icon(Icons.chevron_right, color: Colors.purple)
+        )
+      ],
     );
   }
 
@@ -64,12 +104,13 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
   buildSegmentedControl() {
     return CupertinoSegmentedControl(
       children: {
-        0: buildSegment("Rewards", rewardsColor),
+        0: buildSegment("Earnings", rewardsColor),
         1: buildSegment("Calories", caloriesColor)
       },
       onValueChanged: (int index) {
         setState(() {
           selectedGraph = index;
+          _analyticsVM.switchGraph(index);
           rewardsColor = index == 0 ? Colors.white : Colors.purple;
           caloriesColor = index == 0 ? Colors.purple : Colors.white;
         });
--- Fitdle/lib/pages/analytics/analytics_vm.dart
+++ Fitdle/lib/pages/analytics/analytics_vm.dart
@@ -1,22 +1,91 @@
 import 'package:fitdle/locator.dart';
-import 'package:fitdle/repository/auth_repository.dart';
+import 'package:fitdle/models/earning.dart';
+import 'package:fitdle/repository/api_response.dart';
+import 'package:fitdle/repository/user_repository.dart';
 import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart';
+import '../../models/chart_data.dart';
 
 class AnalyticsVM extends ChangeNotifier {
-  late final AuthRepository _authRepo;
+  late final UserRepository _userRepo;
+  late DateTime _startDate;
+  late DateTime _endDate;
+  late List<ChartData> _selectedData;
+  var _options = [];
+  List<ChartData> _earningData = [
+    ChartData('MON', 12),
+    ChartData('TUE', 15),
+    ChartData('WED', 30),
+    ChartData('THU', 6),
+    ChartData('FRI', 14),
+    ChartData('SAT', 3),
+    ChartData('SUN', 17),
+  ];
+  List<ChartData> _calorieData = [
+    ChartData('MON', 3),
+    ChartData('TUE', 2),
+    ChartData('WED', 5),
+    ChartData('THU', 6),
+    ChartData('FRI', 14),
+    ChartData('SAT', 9),
+    ChartData('SUN', 0),
+  ];
 
-  final PublishSubject _subject = PublishSubject();
+  final PublishSubject _error = PublishSubject();
 
-  PublishSubject get subject => _subject;
+  PublishSubject get error => _error;
+  List<ChartData> get selectedData => _selectedData;
+  String get startDate => "${_startDate.year}/${_startDate.month}/${_startDate.day}";
+  String get endDate => "${_endDate.year}/${_endDate.month}/${_endDate.day}";
 
-  AnalyticsVM([authRepo]) {
-    _authRepo = authRepo ?? locator.get<AuthRepository>();
+  AnalyticsVM([userRepo]) {
+    _userRepo = userRepo ?? locator.get<UserRepository>();
+    _selectedData = _earningData;
+    _options = [_earningData, _calorieData];
+    // Get the monday of the week
+    // weekday is an index, monday is weekday=1 so 1-1 will give now.subtract(0).
+    // which is still monday
+    var now = DateTime.now();
+    _startDate = now.subtract(Duration(days: now.weekday - 1));
+    _endDate = _startDate.add(Duration(days: 6));
   }
 
   @override
   void dispose() {
     super.dispose();
-    _subject.close();
+    _error.close();
+  }
+
+  switchGraph(int index) {
+    _selectedData = _options[index];
+  }
+
+  moveDateBack() {
+    _startDate = _startDate.subtract(Duration(days: 7));
+    _endDate = _endDate.subtract(Duration(days: 7));
+  }
+
+  moveDateForward() {
+    _startDate = _startDate.add(Duration(days: 7));
+    _endDate = _endDate.add(Duration(days: 7));
+  }
+
+  Future<void> getEarningData() async {
+    var res = await _userRepo.fetchEarnings(startDate, endDate);
+    if(res is Success) {
+      var earningsJson = res.data as List;
+      List<Earning> earnings = earningsJson.map((json) => Earning.fromJson(json)).toList();
+      for(Earning earning in earnings) {
+        // TODO: sort earnings into _earningData
+        // There can multiple earning per day so remember to accumulate
+      }
+    } else {
+      res = res as Failure;
+      _error.add(res.data);
+    }
+  }
+
+  Future<void> getCalorieData() async {
+    // TODO: Fetch calorie data
   }
 }
